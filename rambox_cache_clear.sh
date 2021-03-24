37762be4590f690199b3a5c16343515b8e6c0296
Jonathan Balloch <jon.balloch@gmail.com>
2021-03-24 09:02:59 -0400

    rambox cache clear from https://github.com/ramboxapp/community-edition/issues/2756

---
+++ rambox_cache_clear.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -eu
+
+cd $HOME/.config/Rambox/Partitions
+for dir in *;do
+    cd $dir
+    printf '%s\n' "learing cache for $dir\n"
+    rm -rf Cache/*
+    rm -rf Code\ Cache/js/*
+    rm -rf Service\ Worker/CacheStorage/*
+    rm -rf Service\ Worker/ScriptCache/*
+    if [[ -d IndexedDB ]]; then
+        find IndexedDB/ | grep /[0-9]*.ldb | sort | head -n-1 | xargs rm -f
+    fi
+    printf '%s\n' "Cache cleared for $dir\n"
+    cd ..
+done
